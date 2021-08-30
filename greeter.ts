2217881483d76bc1a1bd853bbf0b65ba4c591be6
nichoth <nichoth@gmail.com>
2021-08-30 15:39:52 -0700

    more stuff

--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,24 @@
-function greeter(person) {
-    return "Hello, " + person;
+class Student {
+    fullName: string;
+
+    constructor(
+        public firstName: string,
+        public middleInitial: string,
+        public lastName: string
+    ) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
 }
- 
-let user = "Jane User";
- 
-document.body.textContent = greeter(user);
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+   
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+   
+let user = new Student("Jane", "M.", "User");
+
+console.log(greeter(user))
